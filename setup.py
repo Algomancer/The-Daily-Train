082954a78a390aeb7e07fe0e2c0424551fbe861f
Adam Hibble <hibble@pm.me>
2023-11-22 18:44:18 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ with open(os.path.join(_PATH_ROOT, "README.md"), encoding="utf-8") as fo:
     readme = fo.read()
 
 setup(
-    name="lit-gpt",
+    name="the-daily-train",
     version="0.1.0",
     description="Training models erryday",
     author="Mancer Labs",
-    url="https://github.com/lightning-AI/lit-gpt",
+    url="https://github.com/Algomancer/The-Daily-Train",
     install_requires=[
         "torch>=2.1.0",
         "lightning @ git+https://github.com/Lightning-AI/lightning@6cbe9ceb560d798892bdae9186291acf9bf5d2e3",
